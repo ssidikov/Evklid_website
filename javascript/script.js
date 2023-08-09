8bcb8fb3cf94bf17be3c78c673a2874ba3e2fc25
ssidikov <sardorbek.sidikov7@gmail.com>
2023-08-09 14:48:55 +0200

    Bootstrap connected

--- javascript/script.js
+++ javascript/script.js
@@ -9,15 +9,3 @@ var swiper = new Swiper('.swiper-container', {
 const headerElement = document.getElementById("header");
 const text = headerElement.textContent;
 headerElement.textContent = ""; // Clear the original text
-
-let index = 0;
-const delay = 50; // Adjust this value to control the typing speed
-function typeWriter() {
-  if (index < text.length) {
-    headerElement.textContent += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, delay);
-  }
-}
-
-typeWriter();
